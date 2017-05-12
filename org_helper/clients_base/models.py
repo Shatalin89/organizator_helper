8f34e9c96094b10d766f292a389bb74d21692139
Shatalin89 <shatalin89@yandex.ru>
2017-05-12 16:54:07 +0800

    add main templates

--- org_helper/clients_base/models.py
+++ org_helper/clients_base/models.py
@@ -3,16 +3,18 @@ from django.contrib.auth.models import User
 from phonenumber_field.modelfields import PhoneNumberField
 
 
-
 class Users(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    vk_id = models.CharField(max_length=30, blank=True)
-    num_telephone = PhoneNumberField(blank=True)
-    date_birthday = models.DateField(blank=True)
+    last_name = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=30)
     middle_name = models.CharField(max_length=30, blank=True)
+    date_birthday = models.DateField(blank=True)
+    phone = PhoneNumberField(blank=True)
+    vk_id = models.URLField(blank=True)
+    fb_id = models.URLField(blank=True)
+    insta_id = models.URLField(blank=True)
 
     def get_fio(self):
-        return '{0} {1}.{2}'.format(self.user.last_name, self.user.first_name[:1], self.middle_name[:1])
+        return '{0} {1}.{2}.'.format(self.last_name, self.first_name[:1], self.middle_name[:1])
 
 
 
